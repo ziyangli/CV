341b6ad743a80b63f1fb0178c2d13064fd9eaa53
Joseph Pan <cs.wzpan@gmail.com>
2013-09-18 12:28:18 +0000

    fix a punctuation error

---
+++ auto/WeizhouPan_cn.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook "WeizhouPan_cn"
+ (lambda ()
+    (TeX-run-style-hooks
+     "babel"
+     "english"
+     "WeizhouPan"
+     "latex2e"
+     "moderncv11"
+     "moderncv"
+     "11pt"
+     "a4paper"
+     "nolmodern")))
+
